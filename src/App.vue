5f033663044e1038b94f3ba34165819c2152dc25
烂泥 <1909293822@qq.com>
2023-01-27 19:17:23 +0800

    style: init project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <RouterLink to="/login">Login</RouterLink>
+    <RouterLink to="/Main">Main</RouterLink>
+    <RouterView></RouterView>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
-</script>
+<script lang="ts" setup></script>
 
 <style lang="less">
 #app {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 方式一
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components' // 配置别名
+  //     }
+  //   }
+  // }
+
+  // 方式二
+  configureWebpack: (config) => {
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, 'src'),
+      components: '@/components'
+    }
+  }
+
+  // 方式三
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components')
+  // }
 })
