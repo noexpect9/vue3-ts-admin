c238b4fda12457e0e716e8c000562bf6bfe09b0a
烂泥 <1909293822@qq.com>
2023-01-27 14:50:28 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,14 +95,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
-  },
-});
+    msg: String
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -110,14 +110,17 @@ export default defineComponent({
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
